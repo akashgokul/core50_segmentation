63bb0f22f52808e8b471c27d0609c647c3ebb873
akashgokul <akashgokul@berkeley.edu>
2020-05-22 12:48:30 -0400

    updating to use batch of img paths

--- delete_background_multimages.py
+++ delete_background_multimages.py
@@ -2,148 +2,148 @@ import numpy
 import PIL.Image
 import PIL
 import os
-# import matplotlib.pyplot as plt
-# import matplotlib.cm as cm
-
-def process_img(images_to_process):
-    # Creating a list of all the images to process
-    img_path = '/home/akash/core50/data/core50_128x128/'
-
-    # SET THESE VALUES BEFORE STARTING
-    background_depth = 218
-    svm_threshold = 0.5 # one class = -2.2
-    dilated = 1
-
-    svm_classes = 2
-
-    dilated_image_lst = []
-    for image in sorted(images_to_process):
-        print('Start processing '+image)
-        image_path = img_path  + image
-        image_d = image.replace('C','D')
-        depth_image_path = '/home/akash/core50/data/home/martin/core50_128x128_DepthMap/' + image_d
-
-        depth_image = numpy.asarray(PIL.Image.open(depth_image_path).convert('LA'))
-        depth_image.setflags(write=1)
-        rgb_image = PIL.Image.open(image_path)
-
-        # use this boolean to decide wheater or not delete the holding hand
-        hand_segmentation = True
-
-        # qui devo crearmi una immagine di 1
-        binary_image = numpy.zeros([128, 128], dtype=numpy.uint8)
-        binary_image.fill(1)
-
-        # deleting pixels belonging to the background
-        for i in range(len(depth_image)):
-            for j in range(len(depth_image[i])):
-                if (depth_image[i][j][0]) < background_depth:
-                    depth_image[i][j][0] = 0
-                if (depth_image[i][j][1]) == 0:
-                    depth_image[i][j][1] = 255
-
-        img = PIL.Image.fromarray(depth_image)
-        img = img.convert('RGB')
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
-        # Taking the respective RGB image and deleting the background
-        pixels = rgb_image.load()
-        width, height = img.size
+# Creating a list of all the images to process
+img_path = '/home/akash/core50/data/core50_128x128/'
+images_to_process = os.listdir(img_path)
+
+# SET THESE VALUES BEFORE STARTING
+background_depth = 218
+svm_threshold = 0.5 # one class = -2.2
+dilated = 1
+
+svm_classes = 2
+
+dilated_image_lst = []
+for image in sorted(images_to_process):
+    print('Start processing '+image)
+    image_path = img_path  + image
+    image_d = image.replace('C','D')
+    depth_image_path = '/home/akash/core50/data/home/martin/core50_128x128_DepthMap/' + image_d
+
+    depth_image = numpy.asarray(PIL.Image.open(depth_image_path).convert('LA'))
+    depth_image.setflags(write=1)
+    rgb_image = PIL.Image.open(image_path)
+
+    # use this boolean to decide wheater or not delete the holding hand
+    hand_segmentation = True
+
+    # qui devo crearmi una immagine di 1
+    binary_image = numpy.zeros([128, 128], dtype=numpy.uint8)
+    binary_image.fill(1)
+
+    # deleting pixels belonging to the background
+    for i in range(len(depth_image)):
+        for j in range(len(depth_image[i])):
+            if (depth_image[i][j][0]) < background_depth:
+                depth_image[i][j][0] = 0
+            if (depth_image[i][j][1]) == 0:
+                depth_image[i][j][1] = 255
+
+    img = PIL.Image.fromarray(depth_image)
+    img = img.convert('RGB')
+
+
+    # Taking the respective RGB image and deleting the background
+    pixels = rgb_image.load()
+    width, height = img.size
+    for x in range(width):
+        for y in range(height):
+            r, g, b = img.getpixel((x, y))
+            if r == 0 & g == 0 & b == 0:
+                pixels[x, y] = (0, 0, 0)
+                #new_image[x,y] = 0
+            else:
+                binary_image[x, y] = 0
+    # rgb_image.save('temp'+image)
+
+
+
+    # Using pre-trained SVM model for detecting pixels belonging to the hand
+    if hand_segmentation:
+
+        import pickle as cPickle
+
+        if svm_classes == 1:
+            with open('/home/akash/core50/core50_segmentation/trained_SVM.pkl', 'rb') as fid:
+                svm = cPickle.load(fid)
+                print('SVM one class loaded correctly')
+        elif svm_classes == 2:
+            with open('/home/akash/core50/core50_segmentation/trained_two_classes_SVM.pkl', 'rb') as fid:
+                svm = cPickle.load(fid)
+                print('SVC loaded correctly')
+
+
+        list_SVM = []
         for x in range(width):
             for y in range(height):
-                r, g, b = img.getpixel((x, y))
-                if r == 0 & g == 0 & b == 0:
+                # print(str(x) + ' ' + str(y))
+                rgb_list = []
+                r, g, b = rgb_image.getpixel((x, y))
+                rgb_list.append(r)
+                rgb_list.append(g)
+                rgb_list.append(b)
+                list_SVM.append(rgb_list)
+
+        print('starting SVM prediction for '+image)
+        pred = svm.decision_function(list_SVM)
+
+
+        i = 0
+        pixels = rgb_image.load()
+        for x in range(width):
+            for y in range(height):
+                if pred[i] >= svm_threshold:
+                    #print('this pixel belongs to the hand')
                     pixels[x, y] = (0, 0, 0)
-                    #new_image[x,y] = 0
-                else:
-                    binary_image[x, y] = 0
-        # rgb_image.save('temp'+image)
+                    binary_image[x, y] = 1
+                i = i + 1
+
+        # rgb_image.save('results/' + image)
+
+        from skimage import morphology
+
+        # l'immagine risultante la passo direttamente alla morfologia
+        # plt.imsave('predilation/' + image, binary_image, cmap=cm.gray)
 
 
+        # Applying morphological operators
+        dilated_image = morphology.binary_dilation(binary_image, morphology.diamond(dilated)).astype(numpy.uint8)
+        dilated_image_lst.append(dilated_image)
+        # saving the final image
+        plt.imsave('dilations/' + image,dilated_image, cmap=cm.gray)
 
-        # Using pre-trained SVM model for detecting pixels belonging to the hand
-        if hand_segmentation:
+        # # applying dilation to the rgb
+        # i = 0
+        # pixels = rgb_image.load()
+        # for x in range(width):
+        #     for y in range(height):
+        #         if dilated_image[x,y] == 1:
+        #             # print('this pixel belongs to the hand')
+        #             pixels[x, y] = (0, 0, 0)
+        #         i = i + 1
 
-            import pickle as cPickle
+        # # rgb_image.save('results/' + image + '__dilated.png')
 
-            if svm_classes == 1:
-                with open('/home/akash/core50/core50_segmentation/trained_SVM.pkl', 'rb') as fid:
-                    svm = cPickle.load(fid)
-                    print('SVM one class loaded correctly')
-            elif svm_classes == 2:
-                with open('/home/akash/core50/core50_segmentation/trained_two_classes_SVM.pkl', 'rb') as fid:
-                    svm = cPickle.load(fid)
-                    print('SVC loaded correctly')
+        # # saving the segmented image
 
+        # i = 0
+        # pixels = rgb_image.load()
+        # for x in range(width):
+        #     for y in range(height):
+        #         r, g, b = img.getpixel((x, y))
+        #         ciccio = pixels[x, y]
+        #         if pixels[x, y] != (0,0,0):
+        #             # se il colore non è nero devo fare un leggero overlay
+        #             r, g, b = img.getpixel((x, y))
+        #             pixels[x,y] = (int(r/2), g, int(b/2))
+        #         i = i + 1
 
-            list_SVM = []
-            for x in range(width):
-                for y in range(height):
-                    # print(str(x) + ' ' + str(y))
-                    rgb_list = []
-                    r, g, b = rgb_image.getpixel((x, y))
-                    rgb_list.append(r)
-                    rgb_list.append(g)
-                    rgb_list.append(b)
-                    list_SVM.append(rgb_list)
+        # rgb_image.save('colors/' + image + '__dilated.png')
 
-            print('starting SVM prediction for '+image)
-            pred = svm.decision_function(list_SVM)
-
-
-            i = 0
-            pixels = rgb_image.load()
-            for x in range(width):
-                for y in range(height):
-                    if pred[i] >= svm_threshold:
-                        #print('this pixel belongs to the hand')
-                        pixels[x, y] = (0, 0, 0)
-                        binary_image[x, y] = 1
-                    i = i + 1
-
-            # rgb_image.save('results/' + image)
-
-            from skimage import morphology
-
-            # l'immagine risultante la passo direttamente alla morfologia
-            # plt.imsave('predilation/' + image, binary_image, cmap=cm.gray)
-
-
-            # Applying morphological operators
-            dilated_image = morphology.binary_dilation(binary_image, morphology.diamond(dilated)).astype(numpy.uint8)
-            dilated_image_lst.append(dilated_image)
-            # saving the final image
-            # plt.imsave('dilations/' + image,dilated_image, cmap=cm.gray)
-
-            # applying dilation to the rgb
-            i = 0
-            pixels = rgb_image.load()
-            for x in range(width):
-                for y in range(height):
-                    if dilated_image[x,y] == 1:
-                        # print('this pixel belongs to the hand')
-                        pixels[x, y] = (0, 0, 0)
-                    i = i + 1
-
-            # rgb_image.save('results/' + image + '__dilated.png')
-
-            # saving the segmented image
-
-            i = 0
-            pixels = rgb_image.load()
-            for x in range(width):
-                for y in range(height):
-                    r, g, b = img.getpixel((x, y))
-                    ciccio = pixels[x, y]
-                    if pixels[x, y] != (0,0,0):
-                        # se il colore non è nero devo fare un leggero overlay
-                        r, g, b = img.getpixel((x, y))
-                        pixels[x,y] = (int(r/2), g, int(b/2))
-                    i = i + 1
-
-            # rgb_image.save('colors/' + image + '__dilated.png')
-
-    return numpy.array(dilated_image_lst)
 
 
 
