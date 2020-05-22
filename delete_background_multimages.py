aa88dc151b20f938fa705970836c684772837b4a
akashgokul <akashgokul@berkeley.edu>
2020-05-22 13:00:00 -0400

    updating to use batch of img paths

--- delete_background_multimages.py
+++ delete_background_multimages.py
@@ -9,6 +9,8 @@ import matplotlib.cm as cm
 # Creating a list of all the images to process
 img_path = '/home/akash/core50/data/core50_128x128/'
 images_to_process = [f for f in os.listdir(img_path) if os.path.isfile(f) and f not in ['labels.pkl',  'LUP.pkl',  'paths.pkl']]
+print(images_to_process)
+print(os.listdir(img_path))
 
 # SET THESE VALUES BEFORE STARTING
 background_depth = 218
