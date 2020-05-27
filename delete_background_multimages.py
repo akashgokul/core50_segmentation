4da8dad2a6a91a340ef3a0888b923545b9ce2a7b
akashgokul <akashgokul@berkeley.edu>
2020-05-27 18:10:07 -0400

    fixing seg code

--- delete_background_multimages.py
+++ delete_background_multimages.py
@@ -166,8 +166,7 @@ def save_seg(img_path):
                     i = i + 1
             
             print("PATH: " + image_path[:-4] + '_seg.png')
-            seg_img = numpy.array(rgb_image)[:,:,:3]
-            plt.imsave(image_path[:-4] + '_newseg.png',seg_img, cmap=cm.gray)
+            plt.imsave(image_path[:-4] + '_newseg.png',rgb_image.convert("L"), cmap=cm.gray)
             # rgb_image.save('colors/' + image + '__dilated.png')
     print("------------")
     print("Skipped Files: " + str(missing_ct))
